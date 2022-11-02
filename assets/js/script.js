741d038a183ac336c214e9db16ae218729aaaba4
John Helder <johnhelderjp@gmail.com>
2022-11-02 14:49:19 -0300

    Criando funcionalidade de fechar e abrir o menu no mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,15 @@
   $body.classList.remove('no-js');
   $body.classList.add('js');
 
-  var $btnMenu = document.querySelector('.header__btnMenu');
-  $btnMenu.removeAttribute('style');
+  
+
+  //Fazer o botão toggle funcionar
+
+  var menu = new Menu({
+    container: '.header__nav',
+    toggleBtn: '.header__btnMenu',
+    widthEnabled: 1024 
+})
 })()
 
  
