b465f5e7fc0b4947cd9ad440b021674dca41a466
John Helder <johnhelderjp@gmail.com>
2022-11-03 21:41:06 -0300

    Criando controle do corousel

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
  (function(){
+  //Controle do menu
   var $body = document.querySelector("body");
   $body.classList.remove('no-js');
   $body.classList.add('js');
@@ -11,6 +12,14 @@
     container: '.header__nav',
     toggleBtn: '.header__btnMenu',
     widthEnabled: 1024 
+})
+
+//Controle do carousel
+var carouselImgs = new Carousel({
+  container: ".laptop_slider .slidershow",
+  itens: "figure",
+  btnPrev: ".prev",
+  btnnext: "next"
 })
 })()
 
